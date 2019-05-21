9f84da84e26dce002c8df7def5dbaec9824b1897
Portno <j.remoundos@dotbydot.gr>
2019-05-21 12:40:52 +0300

    unmanaged tabs

--- src/Panacea.Modularity.WebBrowsing/IWebBrowserPlugin.cs
+++ src/Panacea.Modularity.WebBrowsing/IWebBrowserPlugin.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Panacea.Modularity.WebBrowsing
 {
     public interface IWebBrowserPlugin:IPlugin
     {
         void OpenUrl(string url);
+
+        FrameworkElement CreateUnmanagedTab(IWebViewManager manager, bool blockDomains = true, List<string> allowedDomains = null);
     }
 }
